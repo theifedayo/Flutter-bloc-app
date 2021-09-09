1be8c70a8a9abb467370e6fa23e1afb88d67ffb1
theifedayo <adesiyanifedayo@gmail.com>
2021-09-09 08:32:12 +0100

    Setup file structure

---
+++ lib/presentation/router.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+
+class Router {
+
+  Route generateRoute(RouteSettings settings){
+
+    switch(settings.name)
+  }
+}
---
+++ lib/presentation/screens/add_todo_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class AddTodoScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Add To do"),)
+    );
+  }
+}
