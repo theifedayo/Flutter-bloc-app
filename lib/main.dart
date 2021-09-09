411d8e929eecab5349b532d7ba29b219f73e9d57
theifedayo <adesiyanifedayo@gmail.com>
2021-09-09 10:08:58 +0100

    Add app router

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc_app/presentation/screens/todos_screen.dart';
+import 'package:flutter_bloc_app/presentation/router.dart';
 
 void main(){
-  runApp(TodoApp());
+  runApp(TodoApp(
+    router: AppRouter(),
+  ));
 }
 
 class TodoApp extends StatelessWidget {
 
+  final AppRouter router;
+
+  const TodoApp({ Key key, this.router}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TodoScreen(),
+      onGenerateRoute: router.generateRoute,
     );
   }
 }
--- lib/presentation/router.dart
+++ lib/presentation/router.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_app/presentation/screens/add_todo_screen.dart';
+import 'package:flutter_bloc_app/presentation/screens/edit_todo_screen.dart';
+import 'package:flutter_bloc_app/presentation/screens/todos_screen.dart';
 
 
-class Router {
+class AppRouter {
 
   Route generateRoute(RouteSettings settings){
 
-    switch(settings.name)
+    switch(settings.name){
+      case "/":
+        return MaterialPageRoute(builder: (_) => TodoScreen());
+      case "/edit_todo":
+        return MaterialPageRoute(builder: (_) => EditTodoScreen());
+      case "/add_todo":
+        return MaterialPageRoute(builder: (_) => AddTodoScreen());
+      default:
+        return null;
+    }
   }
 }
