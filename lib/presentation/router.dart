f20b4c80b2c21736e5ffff01ceadc9c55beb2c3a
theifedayo <adesiyanifedayo@gmail.com>
2021-09-09 21:05:01 +0100

    Add routes constant

--- lib/presentation/router.dart
+++ lib/presentation/router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_app/constants/strings.dart';
 import 'package:flutter_bloc_app/presentation/screens/add_todo_screen.dart';
 import 'package:flutter_bloc_app/presentation/screens/edit_todo_screen.dart';
 import 'package:flutter_bloc_app/presentation/screens/todos_screen.dart';
@@ -12,9 +13,9 @@ class AppRouter {
     switch(settings.name){
       case "/":
         return MaterialPageRoute(builder: (_) => TodoScreen());
-      case "/edit_todo":
+      case EDIT_TODO_ROUTE:
         return MaterialPageRoute(builder: (_) => EditTodoScreen());
-      case "/add_todo":
+      case ADD_TODO_ROUTE:
         return MaterialPageRoute(builder: (_) => AddTodoScreen());
       default:
         return null;
