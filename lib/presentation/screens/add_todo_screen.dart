a56ba3b341a19c7574f3104e745e8642f2c4e7c5
theifedayo <adesiyanifedayo@gmail.com>
2021-09-22 22:15:46 +0100

    Pub get Toast

--- lib/presentation/screens/add_todo_screen.dart
+++ lib/presentation/screens/add_todo_screen.dart
@@ -1,27 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_app/cubit/add_todo_cubit.dart';
-
-
+import 'package:toast/toast.dart';
 
 class AddTodoScreen extends StatelessWidget {
-
   final _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Add Todo"),
-      ),
-      body: Container(
-        margin: EdgeInsets.all(20),
-        child: _body(context),
-      )
-    );
+        appBar: AppBar(
+          title: Text("Add Todo"),
+        ),
+        body: BlocListener<AddTodoCubit, AddTodoState>(
+          listener: (context, state) {
+            if(state is TodoAdded){
+              Navigator.pop(context);
+            } else if (state is AddTodoError){
+              Toast.show(
+                  state.error,
+                  context,
+                  duration: 3,
+                  backgroundColor: Colors.red,
+                  gravity: Toast.CENTER
+              );
+            }
+          },
+          child: Container(
+            margin: EdgeInsets.all(20),
+            child: _body(context),
+          ),
+        ));
   }
 
-  Widget _body(context){
+  Widget _body(context) {
     return Column(
       children: [
         TextField(
@@ -29,31 +41,37 @@ class AddTodoScreen extends StatelessWidget {
           controller: _controller,
           decoration: InputDecoration(hintText: "Enter todo message"),
         ),
-        SizedBox(height: 10,),
+        SizedBox(
+          height: 10,
+        ),
         GestureDetector(
-          onTap: (){
-            final message = _controller.text;
-            BlocProvider.of<AddTodoCubit>(context).addTodo(message);
-          },
-            child: _addButton(context)
-        )
+            onTap: () {
+              final message = _controller.text;
+              BlocProvider.of<AddTodoCubit>(context).addTodo(message);
+            },
+            child: _addButton(context))
       ],
     );
   }
 
-  Widget _addButton(context){
+  Widget _addButton(context) {
     return Container(
       width: MediaQuery.of(context).size.width,
       height: 50,
       decoration: BoxDecoration(
-        color: Colors.blue,
-        borderRadius: BorderRadius.circular(12)
-      ),
+          color: Colors.blue, borderRadius: BorderRadius.circular(12)),
       child: Center(
-        child: Text("Add Tdo", style: TextStyle(
-          color: Colors.white,
-          fontSize: 18
-        ),),
+        child: BlocBuilder<AddTodoCubit, AddTodoState>(
+          builder: (context, state) {
+            if (state is AddingTodo) {
+              return CircularProgressIndicator();
+            }
+            return Text(
+              "Add Tdo",
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            );
+          },
+        ),
       ),
     );
   }
